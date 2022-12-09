cc38210fd243c5e3b47f1c20943020fc96d25541
Kassy_Francis <89107475+kassy-francis@users.noreply.github.com>
2022-12-09 17:59:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// function update(){
+//     var val = document.getElementById('description').value.length;
+//     document.getElementById('textcount').innerHTML=val + "of 100 characters"
+// }
+
+function update(){
+    var val = document.getElementById('user').value.length;
+    document.getElementById('label').innerHTML = val + 'character(s) have been typed';
+   
+}
+
+
+ 
